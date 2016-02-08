337da0fdc3bff4c940049da83defc0e30996be1a
hugobarzano <hugobarzano@gmail.com>
2016-02-08 19:18:12 +0100

    vagrant y ansible

---
+++ deploy_docker.sh
@@ -0,0 +1,10 @@
+echo Instalando docker y docker-compose
+sudo -i apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-key$
+sudo -i echo /etc/apt/sources.list.d/docker.list deb https://apt.dockerproject.$
+sudo -i apt-get update
+sudo -i apt-get -y install curl
+curl -sSL https://get.docker.com/gpg | sudo apt-key add -
+curl -sSL https://get.docker.com/ | sh
+sudo -i apt-get -y install python-pip
+sudo -i pip install docker-compose
+sudo -i pip install docker-py
