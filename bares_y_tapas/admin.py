08c0babbfe1bef93061abf7364c9888748fef625
hugobarzano <hugobarzano@gmail.com>
2016-02-08 15:18:12 +0100

    modficando

--- bares_y_tapas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from bares_y_tapas.models import *
-
-class BarAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('nombre',)}
-
-admin.site.register(Bar, BarAdmin)
-admin.site.register(Tapa)
