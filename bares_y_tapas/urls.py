264b3f6a3d3aa293eb4e826ad4893c14068a1c30
hugobarzano <hugobarzano@gmail.com>
2016-02-10 09:11:47 +0100

    urls

--- bares_y_tapas/urls.py
+++ bares_y_tapas/urls.py
@@ -3,6 +3,7 @@ from bares_y_tapas import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),#creo url para index y la aniado al nuevo fichero de urls de la aplicacion, asilandolas de
+	url(r'^bares/$', views.index, name='index'),
 	url(r'^about/', views.about, name='about'),
 	url(r'^todos/', views.todos, name='todos'),
     url(r'^details/(?P<nombre_bar>[\w\-]+)/$', views.details, name='details'),
